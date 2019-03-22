4b8160b57bb6ade7205e52f0b940e4da41cf7e53
1daw-26 <1daw-26@PC26>
2019-03-22 08:46:55 +0100

    Commit inicial

---
+++ prJava100/src/prJava100/HolaMundo.java
@@ -0,0 +1,10 @@
+package prJava100;
+
+public class HolaMundo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola mundo, probando repositorios en GitHub");
+	}
+
+}
