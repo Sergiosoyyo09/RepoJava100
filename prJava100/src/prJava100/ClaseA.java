e565443530463d5901df1652092778d215ba76bc
1daw-26 <1daw-26@PC26>
2019-03-22 08:56:05 +0100

    Modifico clase HolaMundo y añado la ClaseA

---
+++ prJava100/src/prJava100/ClaseA.java
@@ -0,0 +1,10 @@
+package prJava100;
+
+public class ClaseA {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Clase nueva");
+	}
+
+}
--- prJava100/src/prJava100/HolaMundo.java
+++ prJava100/src/prJava100/HolaMundo.java
@@ -5,6 +5,7 @@ public class HolaMundo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola mundo, probando repositorios en GitHub");
+		System.out.println("Modifico esta clase");
 	}
 
 }
